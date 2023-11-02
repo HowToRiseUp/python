b48eac5488d846c97304405edef3b0041be2123d
Lê Nhật Hào <501210286@stu.itc.edu.vn>
2023-11-02 20:59:54 +0700

    :)

--- challenge1.py
+++ challenge1.py
@@ -1,3 +1,5 @@
-# in ra 123
-# tạo 1 biến 
-# comment
+print(123)
+name = "Hào"
+year_born = 20; # unused variable
+# print(year_born)
+print(name)
---
+++ readme.md
@@ -0,0 +1,5 @@
+Challenge 1: 
+ - in ra biến 123
+ - tạo biến name và year_born
+ - comment với Python
+ 
